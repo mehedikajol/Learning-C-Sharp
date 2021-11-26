f44cb9e331f4cddbaa70baae3714a2ea090e69af
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2021-11-26 21:09:03 +0600

    Day Three

---
+++ LearningCSharp/App-003-Operators/Program.cs
@@ -0,0 +1,38 @@
+﻿//Operators
+
+
+// Arithmetic Operators + - * / %
+/*
+#region Arithmetic Operators
+var number1 = 45;
+var number2 = 20;
+
+Console.WriteLine(number1 + number2); // Addition
+Console.WriteLine(number1 - number2); // Subtraction   
+Console.WriteLine(number1 * number2); // Multiplication
+Console.WriteLine(number1 / number2); // Division
+Console.WriteLine(number1 % number2); // Reminder
+
+#endregion
+*/
+
+// Relational Operators == != > >= < <=
+#region Relational Operators
+var number1 = 45;
+var number2 = 0b10100;
+
+if (number1 == number2)
+    Console.WriteLine("Two numbers are equal");
+if (number1 != number2)
+    Console.WriteLine("Two numbers are not equal");
+if (number1 > number2)
+    Console.WriteLine("Number1 is greater than number2");
+if (number1 >= number2)
+    Console.WriteLine("Number1 is greater or equal to number2");
+if (number1 < number2)
+    Console.WriteLine("Number1 is smaller than number2");
+if (number1 <= number2)
+    Console.WriteLine("Number1 is smaller or equal to number2");
+
+#endregion
+
