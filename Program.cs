3c0bfc57f84f00ca859d04e5867dad7c3e57552f
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-11 13:41:53 +0600

    day one files

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("This is really fun learning new language.");
+Console.Write("Please let me know your name: ");
+var name = Console.ReadLine();
+Console.Write("Can I know your age please (int): ");
+var age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Hi {name}, you\'re {age} years old");
