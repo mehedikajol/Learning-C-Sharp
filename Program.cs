08fe433a916a10a3d82809148dacbe7193d29936
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-11 14:30:02 +0600

    day two

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int x = 5;
+double y = 6.5;
+string z = "temp";
+bool a = true;
+char c = 'a';
+
+Console.WriteLine(a);
+Console.WriteLine(c);
+Console.WriteLine(z);
+Console.WriteLine(x);
+Console.WriteLine(y);
