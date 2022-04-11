6cab1a53939d93f06ce96365ec0b246f30481602
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-11 16:05:40 +0600

    class intro

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Day three");
+﻿namespace DayThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person kajol = new Person();
+            kajol.name = "Mehedi Hasan Kajol";
+            kajol.age = 26;
+            kajol.weight = 64.85;
+            kajol.height = 187.3;
+            Console.WriteLine($"Name: {kajol.name}, Age: {kajol.age}, Weight: {kajol.weight}, Height: {kajol.height}");
+            /*Console.WriteLine(kajol.name);
+            Console.WriteLine(kajol.age);
+            Console.WriteLine(kajol.height);
+            Console.WriteLine(kajol.weight);*/
+        }
+    }
+}
