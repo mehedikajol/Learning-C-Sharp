33cb4f0b01d5a1c31757938a54ca065064e6573d
Mehedi Hasan Kajol <mehedikajol@users.noreply.github.com>
2022-04-11 15:26:06 +0600

    Delete Program.cs

--- LearningCSharp/App-001-LearningBasics/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.Write("This is really fun learning new language.\nPlease let me know your name: ");
-var name = Console.ReadLine();
-Console.Write("Can I know your age please: ");
-var age = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Hi {name}, it\' nice to meet you and knowing that you\'re {age} years old");
