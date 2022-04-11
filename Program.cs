f72e40f44aec6928b019cbc6888b051debc70f2e
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-11 14:35:34 +0600

    day types with var

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿int x = 5;
+﻿// DATA TYPES
+/*
+int x = 5;
 double y = 6.5;
 string z = "temp";
 bool a = true;
@@ -9,3 +11,19 @@ Console.WriteLine(c);
 Console.WriteLine(z);
 Console.WriteLine(x);
 Console.WriteLine(y);
+*/
+
+// DATA TYPES USING VAR
+/*
+var a = 5;
+var b = 5.4;
+var c = 'v';
+var d = false;
+var e = "this is a string";
+
+Console.WriteLine(e);
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+Console.WriteLine(a);
+*/
