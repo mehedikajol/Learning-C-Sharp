272e67c83457baa608a3968b0d72f3f3ad1465fd
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-15 01:44:03 +0600

    day five

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DayFive
+{
+    public class WaterBottle
+    {
+        public int Capacity { get; }   
+        public string Color { get; }
+        public int WaterAmount { set;get }  
+
+        public WaterBottle(int capacity, string color)
+        {
+            Capacity = capacity;
+            Color = color;
+        }
+
+        public void AddWater(int amount)
+        {
+
+        }
+
+        public void RemoveWater(int amount)
+        {
+
+        }
+    }
+}
