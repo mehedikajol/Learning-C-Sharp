beaae49a186ba0081588edc8cfbe44c42cdc5f8f
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-15 17:42:58 +0600

    generics example

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,8 +4,31 @@
     {
         static void Main(string[] args)
         {
+            //var box = new Box<Type>();
+
             var box1 = new Box<int>();
             var box2 = new Box<double>();
+
+            box1.Length = 12;
+            box1.Width = 10;
+            box1.Height = 8;
+            //Console.WriteLine("Box 1 Volume: " + box1.Length* box1.Width* box1.Height);
+            //Console.WriteLine("Box 1 Area: " + box1.Length * box1.Width);
+
+            var storage = new Storage<string>(5);
+
+            storage.PutItem(0, "This");
+            storage.PutItem(1, "is");
+            storage.PutItem(2, "just");
+            storage.PutItem(3, "an");
+            storage.PutItem(4, "example");
+
+            storage.ShowItems();
+
+            var storage2 = new Storage<int>(2);
+            storage2.PutItem(0, 10);
+            storage2.PutItem(1, 10);
+            storage2.ShowItems();
 
         }
     }
---
+++ Generics/Storage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Storage<T>
+    {
+        private T[] _items;
+
+        public Storage(int size)
+        {
+            _items = new T[size];
+        }
+
+        public void PutItem(int index, T value)
+        {
+            _items[index] = value;
+        }
+
+        public void ShowItems()
+        {
+            foreach (var item in _items)
+                Console.Write(item + " ");
+            Console.WriteLine();
+        }
+
+    }
+}
