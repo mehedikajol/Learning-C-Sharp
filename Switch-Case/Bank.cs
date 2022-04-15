eda524fb4738f61bd15e7c92138287c50b3ef829
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-15 14:56:49 +0600

    enum

---
+++ Switch-Case/Bank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Case
+{
+    public class Bank
+    {
+        public BankBranchStatus status{set; get;}
+        
+        public void PrintBankStatus(BankBranchStatus bankStatus)
+        {
+            if (bankStatus == BankBranchStatus.Open)
+                Console.WriteLine("Bank is open now");
+            else if (bankStatus == BankBranchStatus.Close)
+                Console.WriteLine("Bank is closed");
+            else if (bankStatus == BankBranchStatus.VIPCustomerOnly)
+                Console.WriteLine("Bank is only open of VIPs");
+            else
+                Console.WriteLine("Invalid Inputs");
+        }
+    }
+
+    public enum BankBranchStatus
+    {
+        Open,
+        Close,
+        VIPCustomerOnly
+    }
+}
