f644421c971de95b8f35d8f5fea2e9828e45d399
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-15 16:15:06 +0600

    learning generics

---
+++ Generics/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Box
+    {
+        public int length { set; get; }
+        public int width { set; get; } 
+        public int height { set; get; }
+
+        public int Area() => length * width;
+        public int Volume() => length * width * height;
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Genetirs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
