7476b2621cda141fe910a0a08804f667098873df
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-22 19:21:49 +0600

    collections arraylist and list

---
+++ CollectionsArrayList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsArrayList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList names = new ArrayList();
+            names.Add("Bangladesh");
+            names.Add("Dhaka");
+            names.Add("Mirpur - 1");
+            names.Add(true);
+            names.Add(23.4);
+
+            foreach (var name in names)
+                Console.WriteLine(name);
+            Console.WriteLine(names.Count + "\n");
+
+            names.RemoveAt(names.Count - 1);
+            foreach (var name in names)
+                Console.WriteLine(name);
+            Console.WriteLine(names.Count + "\n");
+
+            List<string> values = new List<string>();
+            values.Add("lorem");
+            values.Add("ipsum");
+            values.Add("dolor");
+            values.Add("sit");
+            values.Add("amet");
+
+            foreach(var value in values)
+                Console.WriteLine(value);
+
+        }
+    }
+}
--- TupleExample/Program.cs
+++ TupleExample/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            
             var time = DateTime.Now;
             var name = "Beautiful Name";
             var person = (name, 26, married: false, 2.6, time);
@@ -18,22 +17,18 @@
             var ListOfPersons = new List<(int age, string name, double weight)>();
 
             for (int i = 1; i <= 10; i++)
-                ListOfPersons.Add((i + 15, $"Mr. {i * 10 + 100}", Math.Round((Double)(i+6)*10.02, 2)));
-            
+                ListOfPersons.Add((i + 15, $"Mr. {i * 10 + 100}", Math.Round((Double)(i + 6) * 10.02, 2)));
+
             foreach (var personDetail in ListOfPersons)
                 Console.WriteLine($"Name: {personDetail.name}, Age: {personDetail.age}, Weight: {personDetail.weight}");
-            
-            
+
             var result = GetPersonResult();
             Console.WriteLine($"Name: {result.name}, Age: {result.age}, Course: {result.result.Subject}, Grade: {result.result.Grade}");
-        
-        }
-        
-        static (string name, int age, Result result) GetPersonResult()
-        {
-            return ("Bangladesh", 51, new Result ( "c-sharp",  3.64 ));
         }
 
-        
+        static (string name, int age, Result result) GetPersonResult()
+        {
+            return ("Bangladesh", 51, new Result("c-sharp", 3.64));
+        }
     }
 }
