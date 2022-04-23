98a28f174a668ac7dd8ea67bd28dae3682c1d48b
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-23 21:39:49 +0600

    file create and delete

--- FileOperation/Program.cs
+++ FileOperation/Program.cs
@@ -13,6 +13,9 @@ namespace FileOperation
         {
             var filepath = @"C:\Users\mehed\OneDrive\Documents\C-Sharp\DayNine\FileOperation\Sample.txt";
             var isExist = File.Exists(filepath);
+            var deleteFilePath = @"C:\Users\mehed\OneDrive\Documents\C-Sharp\DayNine\FileOperation\Delete.txt";
+            var isDeleteExist = File.Exists(deleteFilePath);
+            File.Create(@"C:\Users\mehed\OneDrive\Documents\C-Sharp\DayNine\FileOperation\NewFile.txt");
 
             if (isExist)
             {
@@ -22,11 +25,15 @@ namespace FileOperation
                 Console.WriteLine();
 
                 var lines = File.ReadAllLines(filepath);
-                foreach(var line in lines)
+                foreach (var line in lines)
                     Console.WriteLine($"==={line}===");
             }
-            File.Create(@"C:\Users\mehed\OneDrive\Documents\C-Sharp\DayNine\FileOperation\NewFile.txt");
-            File.Delete(@"C:\Users\mehed\OneDrive\Documents\C-Sharp\DayNine\FileOperation\Delete.txt");
+
+            if (isDeleteExist)
+                File.Delete(deleteFilePath);
+            else
+                Console.WriteLine("File not exist to be deleted");
+
         }
     }
 }
