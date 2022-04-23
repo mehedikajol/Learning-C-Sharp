25e48cd2b4aecdad1fa89c475c0f00c0fa7d04df
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-24 05:39:12 +0600

    more on files

---
+++ DirectoryOperation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryOperation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory(); 
+            Console.WriteLine(currentDirectory);
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(currentDirectory);
+            var filepath = directoryInfo.FullName;
+            var filepathP = directoryInfo.Parent.FullName;
+            var filepathPP = directoryInfo.Parent.Parent.FullName;
+            var filepathPPP = directoryInfo.Parent.Parent.Parent.FullName;
+            var fileName = filepathPPP + @"\newText.txt";
+            var newFileName = Path.Combine(filepathPPP, "newFile.txt");
+
+            Console.WriteLine(filepath);
+            Console.WriteLine(filepathP);
+            Console.WriteLine(filepathPP);
+            Console.WriteLine(filepathPPP);
+
+            File.WriteAllText(fileName, "Hello from Directory Operation");
+            File.WriteAllText(newFileName, "Hello from Path.Combine file");
+            Console.WriteLine();
+
+            var files = directoryInfo.Parent.Parent.Parent.GetFiles();
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Name: {file}, Size: {file.Length}, {file.GetHashCode()}");
+            }
+        }
+    }
+}
+
+
---
+++ FileWrite/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+
+namespace FileWrite
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var writePath = @"C:\Users\mehed\OneDrive\Documents\C-Sharp\DayTen\FileWrite\WriteFile.txt";
+            var content = "Hello from FileWrite learning";
+            File.WriteAllText(writePath, content);
+
+            using var writer = File.OpenWrite(writePath);
+
+            var writePath2 = @"C:\Users\mehed\OneDrive\Documents\C-Sharp\DayTen\FileWrite\WriteFile2.txt";
+            var content2 = "This is not so fun learning C-Sharp";
+            File.WriteAllText(writePath2, content2);
+            var bytes = Encoding.ASCII.GetBytes(content2);
+            writer.Write(bytes);
+
+        }
+    }
+}
