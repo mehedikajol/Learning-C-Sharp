22d2abb5ba20730593301b6839a28f286f9faddd
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-24 23:06:39 +0600

    day twelve starts

---
+++ ADONetPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADONetPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ADO Net Practice");
+        }
+    }
+}
---
+++ DayTwelve/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DayTwelve
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day Twelve");
+        }
+    }
+}
