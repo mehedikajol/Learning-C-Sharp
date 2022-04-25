4ed279a5d898a0ec4177e3775ba870c64b87f7b0
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-25 15:37:11 +0600

    Day 13 starts

---
+++ DayThirteen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DayThirteen
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 13 starts!");
+        }
+    }
+}
