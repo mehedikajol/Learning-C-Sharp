d6579c6e7e6774a7559eb760b249426808c9a5f3
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-25 15:54:24 +0600

    Entity Framework

---
+++ EntityFramework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entity Framework!");
+        }
+    }
+}
